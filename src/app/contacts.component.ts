774ad0e206558949bb8019e0ed7f1c64781780f4
Christoph Burgdorf <christoph.burgdorf@gmail.com>
2016-05-15 22:47:44 -0700

    step 3: display list of contacts using ngFor

--- src/app/contacts.component.ts
+++ src/app/contacts.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Contact } from './models/contact';
+import { CONTACT_DATA } from './data/contact-data';
 
 @Component({
   selector: 'trm-contacts-app',
@@ -8,19 +9,5 @@ import { Contact } from './models/contact';
 })
 export class ContactsAppComponent {
 
-  contact: Contact = {
-    id: 7,
-    name: 'Diana Ellis',
-    email: '',
-    phone: '',
-    birthday: '',
-    website: '',
-    image: '/assets/images/6.jpg',
-    address: {
-      street: '6554 park lane',
-      zip: '43378',
-      city: 'Rush',
-      country: 'United States'
-    }
-  };
+  contacts: Array<Contact> = CONTACT_DATA;
 }
