db1500faaaf2ef16b1252def3aff15b8750f7c34
Christoph Burgdorf <christoph.burgdorf@gmail.com>
2016-05-15 22:27:36 -0700

    step 2: display first contact

--- src/app/contacts.component.ts
+++ src/app/contacts.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Contact } from './models/contact';
 
 @Component({
   selector: 'trm-contacts-app',
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class ContactsAppComponent {
 
+  contact: Contact = {
+    id: 7,
+    name: 'Diana Ellis',
+    email: '',
+    phone: '',
+    birthday: '',
+    website: '',
+    image: '/assets/images/6.jpg',
+    address: {
+      street: '6554 park lane',
+      zip: '43378',
+      city: 'Rush',
+      country: 'United States'
+    }
+  };
 }
