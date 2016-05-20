8f3f303d4e3503f41111751185f3381d3b042d6c
Christoph Burgdorf <christoph.burgdorf@gmail.com>
2016-05-20 11:39:03 +0200

    step 12: refactor to use AsyncPipe in ContactsListComponent

--- src/app/contacts-list/contacts-list.component.ts
+++ src/app/contacts-list/contacts-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
 import { ContactsService } from '../contacts.service';
 import { Contact } from '../models/contact';
 
@@ -9,13 +10,12 @@ import { Contact } from '../models/contact';
 })
 export class ContactsListComponent implements OnInit {
 
-  contacts: Array<Contact>;
+  contacts: Observable<Array<Contact>>;
 
   constructor(private contactsService: ContactsService) {}
 
   ngOnInit() {
-    this.contactsService.getContacts()
-                        .subscribe(contacts => this.contacts = contacts);
+    this.contacts = this.contactsService.getContacts();
   }
 
 }
