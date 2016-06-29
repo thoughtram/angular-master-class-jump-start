2796fcdcfc84ed211ab5a4601cf6e74d9bbdebf3
Pascal Precht <pascal.precht@googlemail.com>
2016-06-29 15:09:52 +0200

    step 9: make API_ENDPOINT injectable

--- src/app/contacts.service.ts
+++ src/app/contacts.service.ts
@@ -1,26 +1,24 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject } from '@angular/core';
 import { Http } from '@angular/http';
 import { Contact } from './models/contact';
 
 @Injectable()
 export class ContactsService {
 
-  private API_ENDPOINT = 'http://localhost:4201/api';
-
-  constructor(private http: Http) {}
+  constructor(private http: Http, @Inject('API_ENDPOINT') private apiEndpoint) {}
 
   getContact(id: number | string) {
-    return this.http.get(`${this.API_ENDPOINT}/contacts/${id}`)
+    return this.http.get(`${this.apiEndpoint}/contacts/${id}`)
                     .map(res => res.json().item);
   }
 
   getContacts() {
-    return this.http.get(`${this.API_ENDPOINT}/contacts`)
+    return this.http.get(`${this.apiEndpoint}/contacts`)
                     .map(res => res.json())
                     .map(data => data.items);
   }
 
   updateContact(contact: Contact) {
-    return this.http.put(`${this.API_ENDPOINT}/contacts/${contact.id}`, contact);
+    return this.http.put(`${this.apiEndpoint}/contacts/${contact.id}`, contact);
   }
 }
