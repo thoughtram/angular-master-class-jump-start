3c9f701bb4a81b603c4af1db07850c70de924574
Pascal Precht <pascal.precht@googlemail.com>
2016-12-30 12:31:09 +0100

    step 2: Display first contact using MatItemList and MatItem

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import { Contact } from './models/contact';
 
 @Component({
   selector: 'trm-contacts-app',
   template: `
     <mat-toolbar color="primary">Contacts</mat-toolbar>
+    <mat-list>
+      <mat-list-item>
+        <img mat-list-avatar [src]="contact.image" alt="Picture of {{contact.name}}" class="circle">
+        <h3 mat-line>{{contact.name}}</h3>
+      </mat-list-item>
+    </mat-list>
   `,
   styleUrls: ['./app.component.scss']
 })
 export class ContactsAppComponent {
-  title = 'Angular Master Class';
+  contact: Contact = {
+    id: 7,
+    name: 'Diana Ellis',
+    email: '',
+    phone: '',
+    birthday: '',
+    website: '',
+    image: '/assets/images/6.jpg',
+    address: {
+      street: '6554 park lane',
+      zip: '43378',
+      city: 'Rush',
+      country: 'United States'
+    }
+  };
 }
