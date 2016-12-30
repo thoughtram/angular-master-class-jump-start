c069c33c24d65426411b79ec65b01fe02eab5a5f
Pascal Precht <pascal.precht@googlemail.com>
2016-12-30 12:41:02 +0100

    step 3: display list of contacts using ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { Contact } from './models/contact';
+import { CONTACT_DATA } from './data/contact-data';
 
 @Component({
   selector: 'trm-contacts-app',
   template: `
     <mat-toolbar color="primary">Contacts</mat-toolbar>
     <mat-list>
-      <mat-list-item>
+      <mat-list-item *ngFor="let contact of contacts">
         <img mat-list-avatar [src]="contact.image" alt="Picture of {{contact.name}}" class="circle">
         <h3 mat-line>{{contact.name}}</h3>
       </mat-list-item>
@@ -15,19 +16,5 @@ import { Contact } from './models/contact';
   styleUrls: ['./app.component.scss']
 })
 export class ContactsAppComponent {
-  contact: Contact = {
-    id: 7,
-    name: 'Diana Ellis',
-    email: '',
-    phone: '',
-    birthday: '',
-    website: '',
-    image: '/assets/images/6.jpg',
-    address: {
-      street: '6554 park lane',
-      zip: '43378',
-      city: 'Rush',
-      country: 'United States'
-    }
-  };
+  contacts: Array<Contact> = CONTACT_DATA;
 }
