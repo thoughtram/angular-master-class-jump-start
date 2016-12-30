b6ca9f8fab263bed3e0f2d8b08b8d30a2c59e6a0
Pascal Precht <pascal.precht@googlemail.com>
2016-12-30 12:57:02 +0100

    step 6: Route to ContactsListComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { Contact } from './models/contact';
-import { ContactsService } from './contacts.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'trm-contacts-app',
   template: `
     <mat-toolbar color="primary">Contacts</mat-toolbar>
-    <mat-list>
-      <mat-list-item *ngFor="let contact of contacts; trackBy:trackByContactId">
-        <img mat-list-avatar [src]="contact.image" alt="Picture of {{contact.name}}" class="circle">
-        <h3 mat-line>{{contact.name}}</h3>
-      </mat-list-item>
-    </mat-list>
+    <router-outlet></router-outlet>
   `,
   styleUrls: ['./app.component.scss']
 })
-export class ContactsAppComponent implements OnInit {
-
-  contacts: Array<Contact>;
-
-  constructor(private contactsService: ContactsService) {}
-
-  ngOnInit () {
-    this.contacts = this.contactsService.getContacts();
-  }
-
-  trackByContactId(index, contact) {
-    return contact.id;
-  }
-}
+export class ContactsAppComponent {}
