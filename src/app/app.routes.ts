1d6e29c9ddb50ff809d8f3983c56ace3523bb630
Pascal Precht <pascal.precht@googlemail.com>
2016-12-30 12:58:16 +0100

    step 7: Add fallback route and redirect to root

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,9 @@ import { Routes } from '@angular/router';
 import { ContactsListComponent } from './contacts-list/contacts-list.component';
 
 export const APP_ROUTES: Routes = [
-  { path: '', component: ContactsListComponent }
+  { path: '', component: ContactsListComponent },
+  // Wildcard route serves as fallback route and has to be
+  // the last defined route in this list.
+  { path: '**', redirectTo: '/' }
 ];
 
