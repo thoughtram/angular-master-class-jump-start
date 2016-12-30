7c89362dc0d60844fd0ec45ba1115b8b83f74d65
Pascal Precht <pascal.precht@googlemail.com>
2016-12-30 12:57:02 +0100

    step 6: Route to ContactsListComponent

---
+++ src/app/app.routes.ts
@@ -0,0 +1,7 @@
+import { Routes } from '@angular/router';
+import { ContactsListComponent } from './contacts-list/contacts-list.component';
+
+export const APP_ROUTES: Routes = [
+  { path: '', component: ContactsListComponent }
+];
+
