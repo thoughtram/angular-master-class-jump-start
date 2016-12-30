3631c940b85004925769c3880128dd1fc228d244
Pascal Precht <pascal.precht@googlemail.com>
2016-12-30 12:57:02 +0100

    step 6: Route to ContactsListComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { RouterModule } from '@angular/router';
+
 import { ContactsMaterialModule } from './contacts-material.module';
 
 import { ContactsAppComponent } from './app.component';
+import { ContactsListComponent } from './contacts-list/contacts-list.component';
+
 import { ContactsService } from './contacts.service';
 
+import { APP_ROUTES } from './app.routes';
 
 @NgModule({
-  declarations: [ContactsAppComponent],
+  declarations: [ContactsAppComponent, ContactsListComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     ContactsMaterialModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    RouterModule.forRoot(APP_ROUTES)
   ],
   providers: [ContactsService],
   bootstrap: [ContactsAppComponent]
