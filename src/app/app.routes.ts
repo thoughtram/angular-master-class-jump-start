da2469bdd81dfc47964e446bde8677d9d0eb9e7b
Pascal Precht <pascal.precht@googlemail.com>
2017-01-02 13:30:07 +0100

    step 8: route to ContactsDetailComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { ContactsListComponent } from './contacts-list/contacts-list.component';
+import { ContactsDetailComponent } from './contacts-detail/contacts-detail.component';
 
 export const APP_ROUTES: Routes = [
   { path: '', component: ContactsListComponent },
+  { path: 'contact/:id', component: ContactsDetailComponent },
   // Wildcard route serves as fallback route and has to be
   // the last defined route in this list.
   { path: '**', redirectTo: '/' }
