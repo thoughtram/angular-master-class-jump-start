4da593f8b06036141cef6e5c5623e902ac7086c6
Pascal Precht <pascal.precht@googlemail.com>
2017-01-02 14:02:17 +0100

    step 11: make API_ENDPOINT an InjectionToken

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ContactsDetailComponent } from './contacts-detail/contacts-detail.compo
 import { ContactsService } from './contacts.service';
 
 import { APP_ROUTES } from './app.routes';
+import { API_ENDPOINT } from './app.tokens';
 
 @NgModule({
   declarations: [
@@ -31,7 +32,7 @@ import { APP_ROUTES } from './app.routes';
   ],
   providers: [
     ContactsService,
-    { provide: 'API_ENDPOINT', useValue: 'http://localhost:4201/api' }
+    { provide: API_ENDPOINT, useValue: 'http://localhost:4201/api' }
   ],
   bootstrap: [ContactsAppComponent]
 })
