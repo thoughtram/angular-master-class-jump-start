831437e18dbf6b085d9eab5e3d65a65b10b9dc06
Pascal Precht <pascal.precht@googlemail.com>
2017-01-02 13:50:31 +0100

    step 9: fetch contact data using HttpClient

--- src/app/contacts.service.ts
+++ src/app/contacts.service.ts
@@ -1,16 +1,31 @@
 import { Injectable } from '@angular/core';
-import { CONTACT_DATA } from './data/contact-data';
+import { HttpClient } from '@angular/common/http';
+
+import { Observable } from 'rxjs/Observable';
+import { map } from 'rxjs/operators';
+
+import { Contact } from './models/contact';
+
+interface ContactResponse  { item  : Contact    }
+interface ContactsResponse { items : Contact[]  }
+
 
 @Injectable()
 export class ContactsService {
 
-  constructor() {}
+  private API_ENDPOINT = 'http://localhost:4201/api';
 
-  getContact (id: string) {
-    return this.getContacts().find(contact => contact.id.toString() === id);
+  constructor(private http: HttpClient) {}
+
+
+  getContact(id: string): Observable<Contact> {
+    return this.http.get<ContactResponse>(`${this.API_ENDPOINT}/contacts/${id}`)
+        .pipe(map(data => data.item));
   }
 
-  getContacts () {
-    return CONTACT_DATA;
+  getContacts(): Observable<Array<Contact>> {
+    return this.http.get<ContactsResponse>(`${this.API_ENDPOINT}/contacts`)
+        .pipe(map(data => data.items));
   }
+
 }
